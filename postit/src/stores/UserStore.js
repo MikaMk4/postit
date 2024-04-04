9772e011bec9dec814f0a1dcdb7d0747a56d3c4d
MikaMk4 <mikailamelek@gmail.com>
2024-04-04 15:30:43 +0200

    added boards and post creation

--- postit/src/stores/UserStore.js
+++ postit/src/stores/UserStore.js
@@ -2,14 +2,59 @@ import { defineStore } from 'pinia'
 
 export const useUserStore = defineStore('userStore', {
     state: () => ({
+        loading: false,
         user: null
     }),
     actions: {
-        login(username) {
+        async login(username, password) {
+            this.loading = true
+
+            const response = await fetch('http://localhost:3000/api/v1/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ username, password })
+            })
+
+            const data = await response.json()
+
+            if (!response.ok) {
+                throw new Error(data.message || 'Failed to login')
+            }
+
             this.user = {
                 name: username,
-                avatar: 'https://cdn.discordapp.com/avatars/452415473687068672/1c6bad1e46a6612c40d20d1ac2f61c7e.webp?size=1024'
+                avatar: 'https://cdn.discordapp.com/avatars/452415473687068672/1c6bad1e46a6612c40d20d1ac2f61c7e.webp?size=1024',
+                token: data.token
             }
+
+            this.loading = false
+        },
+        async register(username, password) {
+            this.loading = true
+
+            const response = await fetch('http://localhost:3000/api/v1/signup', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ username, password })
+            })
+
+            const data = await response.json()
+
+            if (!response.ok) {
+                throw new Error(data.message || 'Failed to register')
+            }
+
+            this.user = {
+                name: username,
+                avatar: 'https://cdn.discordapp.com/avatars/452415473687068672/1c6bad1e46a6612c40d20d1ac2f61c7e.webp?size=1024',
+                token: data.token
+            }
+
+            this.loading = false
         },
         logout() {
             this.user = null
